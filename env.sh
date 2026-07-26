183652e1865b09d7a6c8795e1e272997e72c08cc
desiato <hbdesiato@gmx.de>
2026-07-26 17:31:39 +0200

    localhost:5000/silverblue-nvidia:main pushed

--- env.sh
+++ env.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 SIGSTORE_PUB=keys/sealedblue.pub
 SIGSTORE_PREFIX=${SIGSTORE_PUB%.*}
-[ ${GITHUB_REPOSITORY-} ] && IMAGE_PREFIX=ghcr.io/${GITHUB_REPOSITORY%/*}
+[ -z ${GITHUB_REPOSITORY-} ] || IMAGE_PREFIX=ghcr.io/${GITHUB_REPOSITORY%/*}
--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 set -euxo pipefail
 . ./env.sh
 
-for INPUT in $(basename -s .digest input/*); do
+INPUT_PREFIX=$(systemd-escape "$IMAGE_PREFIX")
+for INPUT in $(basename -s .digest input/${INPUT_PREFIX}*); do
     DIGEST1=$(cat "input/${INPUT}.digest")
     export BASE_IMAGE=$(systemd-escape -u "$INPUT")
-    podman pull $BASE_IMAGE
-    DIGEST2=$(podman inspect --format {{.Digest}} $BASE_IMAGE)
+    DIGEST2=$(skopeo inspect --format='{{.Digest}}' "docker://${BASE_IMAGE}")
     [ "$DIGEST1" == "$DIGEST2" ] || ./build.sh
 done
